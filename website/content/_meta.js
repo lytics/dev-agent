0926ca1ee9f04b5e8bedba80c4c791bd03d7b4eb
prosdev <prosdev@users.noreply.github.com>
2025-11-29 05:27:35 -0800

    docs(website): remove blog section, link to devbypros.com

--- website/content/_meta.js
+++ website/content/_meta.js
@@ -1,5 +1,4 @@
 export default {
   index: 'Home',
   docs: 'Documentation',
-  blog: 'Blog',
 };
--- website/content/blog/_meta.js
+++
@@ -1,4 +0,0 @@
-export default {
-  index: 'Blog',
-  '10-days-of-vibe-coding': '10 days of vibe coding',
-};
